92c8fc8feb3e1abdf20b2e2addb3f4e14e2b6c68
Naspry <nazarsprynskyy2004@gmail.com>
2025-03-26 17:46:19 +0100

    Aggiunta descrizione A IOConsole

--- src/it/uniroma3/diadia/IOConsole.java
+++ src/it/uniroma3/diadia/IOConsole.java
@@ -1,6 +1,12 @@
 package it.uniroma3.diadia;
 import java.util.Scanner;
 
+/** 
+ * La classe si occupa di gestire l'interfaccia con l'utente.
+ * 
+ * @author Docente di POO
+ */
+
 public class IOConsole {
 	public void mostraMessaggio(String msg) {
 		System.out.println(msg);
