abd737445e27b4ea9fdc00b515cf51dd77f505af
Naspry <141187998+Naspry@users.noreply.github.com>
2025-05-02 17:58:54 +0200

    piccoli ritocchi

--- src/it/uniroma3/diadia/IOSimulator.java
+++ src/it/uniroma3/diadia/IOSimulator.java
@@ -4,6 +4,9 @@ public class IOSimulator implements IO {
 
 	private String[] comandi;
 	int i = 0;
+	
+	final String ANSI_RESET = "\u001B[0m";
+	final String ANSI_GREEN = "\u001B[32m";
 
 	public IOSimulator(String[] comandi) {
 		this.comandi = comandi;
@@ -12,14 +15,15 @@ public class IOSimulator implements IO {
 
 	@Override
 	public void mostraMessaggio(String messaggio) {
-		System.out.println(messaggio);
-		System.out.println("----------------------------------------");
+		System.out.println(messaggio + '\n');
+		
 	
 	}
 
 	@Override
 	public String leggiRiga() {
 		if(i<comandi.length) {
+			this.mostraMessaggio(ANSI_GREEN + comandi[i] + ANSI_RESET);
 			return this.comandi[i++];
 		}
 		return null;
--- test/it/uniroma3/diadia/IOSimulatorTest.java
+++ test/it/uniroma3/diadia/IOSimulatorTest.java
@@ -2,7 +2,6 @@ package it.uniroma3.diadia;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IOSimulatorTest {
@@ -14,7 +13,7 @@ class IOSimulatorTest {
 	void testVittoria() {
 		String vittoria[] = {
 				"vai nord", "vai ovest", "prendi chiave", "vai ovest", 
-				"prendi lanterna", "vai est", "vai nord", "posa lanterna",
+				"prendi lanterna", "vai est", "vai nord", "posa lanterna", "guarda",
 				"vai nord", "posa chiave", "vai nord", "vai nord"
 				};
 		
